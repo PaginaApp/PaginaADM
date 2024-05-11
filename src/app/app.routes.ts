f9cb7e731481168e7deb686bad00b2b060b6380b
nathan ks <nathanks0210@gmail.com>
2024-05-11 18:52:19 -0300

    feat: Add route for MyAccountComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MyAccountComponent } from './components/my-account/my-account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'conta', component: MyAccountComponent, pathMatch: 'full' },
+];
