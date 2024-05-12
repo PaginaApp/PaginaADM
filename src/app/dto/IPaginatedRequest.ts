e1df0e3bc8d4617844310dbcf1e1a55e345e68bb
nathan ks <nathanks0210@gmail.com>
2024-05-12 17:49:10 -0300

    refactor: Update IPaginatedRequest constructor to accept optional filter parameter

--- src/app/dto/IPaginatedRequest.ts
+++ src/app/dto/IPaginatedRequest.ts
@@ -1,12 +1,14 @@
 class IPaginatedRequest<T> {
   page: number;
   limit: number;
-  filter: Partial<T>;
+  filter?: Partial<T>;
 
-  constructor(page: number, limit: number, filter: Partial<T>) {
+  constructor(page: number, limit: number, filter?: Partial<T>) {
     this.page = page;
     this.limit = limit;
-    this.filter = filter;
+    if (filter) {
+      this.filter = filter;
+    }
   }
 }
 
