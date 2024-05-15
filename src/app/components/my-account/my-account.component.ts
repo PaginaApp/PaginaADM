1cb20ca81a1f6b8415f98d21b6329e0bd245e797
nathan ks <nathanks0210@gmail.com>
2024-05-15 20:52:35 -0300

    refactor: Update MyAccountComponent to use lazy loading for avatar image

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AvatarService } from '../../_services/Avatar/avatar.service';
+import { ErrorDTO } from '../../dto/ErrorDTO';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
@@ -9,7 +12,34 @@ import { HeaderComponent } from '../header/header.component';
   styleUrl: './my-account.component.css',
 })
 export class MyAccountComponent implements OnInit {
-  constructor() {}
+  constructor(private avatarService: AvatarService, private router: Router) {}
 
-  ngOnInit(): void {}
+  public avatar: string = '';
+
+  async ngOnInit(): Promise<void> {
+    // seta o avatar do usuário
+    await this.setAvatar();
+  }
+
+  private async setAvatar(): Promise<void> {
+    if (sessionStorage.getItem('user') === null) {
+      this.router.navigate(['/login']);
+      return;
+    }
+    let user = JSON.parse(sessionStorage.getItem('user')!);
+
+    try {
+      const resposta = await this.avatarService.getAvatar(
+        user.usu_Id.toString()
+      );
+
+      if (resposta instanceof ErrorDTO) {
+        window.alert(resposta.message);
+      } else {
+        this.avatar = resposta;
+      }
+    } catch (error) {
+      window.alert('Erro ao chamar getAvatar');
+    }
+  }
 }
