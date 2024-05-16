597c811eb35c57b2c3958fa35ac246ac460a8159
nathan ks <nathanks0210@gmail.com>
2024-05-16 19:42:05 -0300

    feat: Implement criarLivro method in LivroService

--- src/app/_services/Livro/livro.service.ts
+++ src/app/_services/Livro/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+import { CreateLivroDTO } from '../../dto/CreateLivroDTO';
 import { ErrorDTO } from '../../dto/ErrorDTO';
 import { IPaginatedResponse } from '../../dto/IPaginatedResponse';
 import { LivroDTO } from '../../dto/LivroDTO';
@@ -38,6 +39,29 @@ export class LivroService {
         return new ErrorDTO(error.error.message, error.status);
       } else {
         return new ErrorDTO('Erro ao chamar listUsers', 500);
+      }
+    }
+  }
+
+  // criar um novo livro
+  async criarLivro(livro: CreateLivroDTO): Promise<LivroDTO | ErrorDTO> {
+    try {
+      const response = await firstValueFrom(
+        this.http.post<any>(`${this.globalService.baseUrl}livro`, livro, {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+            // para pular a mensagem do navegador
+            'ngrok-skip-browser-warning': 'true',
+          },
+        })
+      );
+
+      return response;
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return new ErrorDTO(error.error.message, error.status);
+      } else {
+        return new ErrorDTO('Erro ao chamar criarLivro', 500);
       }
     }
   }
