426c2e5a3bc9f870b5ae895ca48e21f02e6407bb
nathan ks <nathanks0210@gmail.com>
2024-05-15 21:58:28 -0300

    refactor: Update MyAccountComponent to add image upload functionality

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -21,6 +21,43 @@ export class MyAccountComponent implements OnInit {
     await this.setAvatar();
   }
 
+  public onFileSelected(event: any): void {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.avatar = reader.result as string;
+        this.newAvatar(this.avatar.split(',')[1]);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  private async newAvatar(avatar: string): Promise<void> {
+    if (sessionStorage.getItem('user') === null) {
+      this.router.navigate(['/login']);
+      return;
+    }
+    let user = JSON.parse(sessionStorage.getItem('user')!);
+
+    try {
+      const resposta = await this.avatarService.setAvatar(
+        user.usu_Id.toString(),
+        avatar
+      );
+
+      if (resposta instanceof ErrorDTO) {
+        window.alert(resposta.message);
+      } else {
+        // resposta não contém nada, então o avatar foi alterado com sucesso e deve ser atualizado na tela
+        this.setAvatar();
+      }
+    } catch (error) {
+      window.alert('Erro ao chamar setAvatar');
+    }
+  }
+
   private async setAvatar(): Promise<void> {
     if (sessionStorage.getItem('user') === null) {
       this.router.navigate(['/login']);
