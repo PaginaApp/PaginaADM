6959c0afe4e9d4e9550877ad2959a9339553538b
nathan ks <nathanks0210@gmail.com>
2024-05-16 16:45:20 -0300

    feat: Add EditoraDTO class for publisher data transfer object

---
+++ src/app/dto/EditoraDTO.ts
@@ -0,0 +1,11 @@
+class EditoraDTO {
+  edi__Id: string;
+  edi_Nome: string;
+
+  constructor(edi_Id: string, edi_Nome: string) {
+    this.edi__Id = edi_Id;
+    this.edi_Nome = edi_Nome;
+  }
+}
+
+export { EditoraDTO };
