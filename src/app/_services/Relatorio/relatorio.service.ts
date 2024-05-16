3ba0fe996433cb08fb54e48a146a60303bc5f7a2
nathan ks <nathanks0210@gmail.com>
2024-05-16 20:14:36 -0300

    feat: Add RelatorioService for generating age report

---
+++ src/app/_services/Relatorio/relatorio.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { ErrorDTO } from '../../dto/ErrorDTO';
+import { RelatorioIdadeDTO } from '../../dto/RelatorioIdadeDTO';
+import { GlobalService } from '../global.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RelatorioService {
+  constructor(private globalService: GlobalService, private http: HttpClient) {}
+
+  // relatório de usuário por idade
+  async relatorioIdade(): Promise<RelatorioIdadeDTO | ErrorDTO> {
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(
+          `${this.globalService.baseUrl}relatorio/users/idade`,
+          {
+            headers: {
+              Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+              // para pular a mensagem do navegador
+              'ngrok-skip-browser-warning': 'true',
+            },
+          }
+        )
+      );
+      return new RelatorioIdadeDTO(response);
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return new ErrorDTO(error.error.message, error.status);
+      } else {
+        return new ErrorDTO('Erro ao chamar listUsers', 500);
+      }
+    }
+  }
+}
