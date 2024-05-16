1551bd0c5221109d8ee2630d85051a6770db5195
nathan ks <nathanks0210@gmail.com>
2024-05-16 20:25:05 -0300

    feat: Add relatorioExemplarMes method to RelatorioService for generating monthly exemplar report

--- src/app/_services/Relatorio/relatorio.service.ts
+++ src/app/_services/Relatorio/relatorio.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { ErrorDTO } from '../../dto/ErrorDTO';
+import { RelatorioExemplarMesDTO } from '../../dto/RelatorioExemplarMesDTO';
 import { RelatorioIdadeDTO } from '../../dto/RelatorioIdadeDTO';
 import { GlobalService } from '../global.service';
 
@@ -27,6 +28,33 @@ export class RelatorioService {
         )
       );
       return new RelatorioIdadeDTO(response);
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return new ErrorDTO(error.error.message, error.status);
+      } else {
+        return new ErrorDTO('Erro ao chamar listUsers', 500);
+      }
+    }
+  }
+
+  // relatório de exemplares cadastrados por mês
+  async relatorioExemplarMes(
+    ano: Number
+  ): Promise<RelatorioExemplarMesDTO | ErrorDTO> {
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(
+          `${this.globalService.baseUrl}relatorio/exemplares/${ano}`,
+          {
+            headers: {
+              Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+              // para pular a mensagem do navegador
+              'ngrok-skip-browser-warning': 'true',
+            },
+          }
+        )
+      );
+      return response;
     } catch (error) {
       if (error instanceof HttpErrorResponse) {
         return new ErrorDTO(error.error.message, error.status);
