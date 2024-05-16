a14a013e123664e074b46ea943d8f6ca84635776
nathan ks <nathanks0210@gmail.com>
2024-05-16 16:25:47 -0300

    feat: Add AutorDTO class for author data transfer object

---
+++ src/app/dto/AutorDTO.ts
@@ -0,0 +1,11 @@
+class AutorDTO {
+  aut_Id: string;
+  aut_Nome: string;
+
+  constructor(aut_Id: string, aut_Nome: string) {
+    this.aut_Id = aut_Id;
+    this.aut_Nome = aut_Nome;
+  }
+}
+
+export { AutorDTO };
