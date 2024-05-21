d9b194f471f4e0537a85f2b8c742f8609f69ad5c
nathan ks <nathanks0210@gmail.com>
2024-05-20 22:43:10 -0300

    refactor: Add livros component with table layout and basic functionality

---
+++ src/app/components/livros/livros/livros.component.ts
@@ -0,0 +1,52 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { LivroService } from '../../../_services/Livro/livro.service';
+import { ErrorDTO } from '../../../dto/ErrorDTO';
+import { IPaginatedResponse } from '../../../dto/IPaginatedResponse';
+import { LivroDTO } from '../../../dto/LivroDTO';
+import { HeaderComponent } from '../../header/header.component';
+
+@Component({
+  selector: 'app-livros',
+  standalone: true,
+  imports: [HeaderComponent, NgFor, NgIf],
+  templateUrl: './livros.component.html',
+  styleUrl: './livros.component.css',
+})
+export class LivrosComponent implements OnInit {
+  constructor(private livroService: LivroService) {}
+
+  public livros: IPaginatedResponse<LivroDTO> = {
+    limit: 9,
+    page: 1,
+    total: 0,
+    results: [],
+  };
+
+  public totalPages(): number {
+    return Math.ceil(this.livros.total / this.livros.limit);
+  }
+
+  public error: ErrorDTO | undefined;
+
+  async ngOnInit(): Promise<void> {
+    await this.updateList();
+  }
+
+  private async updateList(): Promise<void> {
+    try {
+      const data = await this.livroService.listarLivros(
+        this.livros.page,
+        this.livros.limit
+      );
+
+      if (data instanceof ErrorDTO) {
+        this.error = data;
+      } else {
+        this.livros = data;
+      }
+    } catch (error) {
+      this.error = new ErrorDTO('Erro ao listar livros', 500);
+    }
+  }
+}
