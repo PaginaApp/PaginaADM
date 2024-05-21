3bd7b4f63e97e0d53fcc0e2e9a37abd848ce067f
nathan ks <nathanks0210@gmail.com>
2024-05-20 22:43:50 -0300

    feat: Add livros component with table layout and basic functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { autenticacaoGuard } from './_services/guards/autenticacao.guard';
+import { LivrosComponent } from './components/livros/livros/livros.component';
 import { LoginComponent } from './components/login/login.component';
 import { MyAccountComponent } from './components/my-account/my-account.component';
 
@@ -12,6 +13,12 @@ export const routes: Routes = [
   {
     path: 'conta',
     component: MyAccountComponent,
+    pathMatch: 'full',
+    canActivate: [autenticacaoGuard],
+  },
+  {
+    path: 'livros',
+    component: LivrosComponent,
     pathMatch: 'full',
     canActivate: [autenticacaoGuard],
   },
