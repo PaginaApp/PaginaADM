892469e6eacf78b469d0cad1fedcf37341c97832
nathan ks <nathanks0210@gmail.com>
2024-05-21 20:07:13 -0300

    feat: Add CategoriaService with list method

---
+++ src/app/_services/Categoria/categoria.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { CategoriaDTO } from '../../dto/CategoriaDTO';
+import { ErrorDTO } from '../../dto/ErrorDTO';
+import { IPaginatedResponse } from '../../dto/IPaginatedResponse';
+import { GlobalService } from '../global.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriaService {
+  constructor(private globalService: GlobalService, private http: HttpClient) {}
+
+  // lista categorias
+  public async list(): Promise<IPaginatedResponse<CategoriaDTO> | ErrorDTO> {
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(`${this.globalService.baseUrl}categoria`, {
+          headers: {
+            // para pular a mensagem do navegador
+            'ngrok-skip-browser-warning': 'true',
+          },
+        })
+      );
+      return response;
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return new ErrorDTO(error.error.message, error.status);
+      } else {
+        return new ErrorDTO('Erro ao chamar listUsers', 500);
+      }
+    }
+  }
+}
