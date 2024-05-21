b2775ae729fc4b6545e9a99d8cf656bc3817080c
nathan ks <nathanks0210@gmail.com>
2024-05-21 19:26:58 -0300

    feat: Add listByName method to EditoraService

--- src/app/_services/Editora/editora.service.ts
+++ src/app/_services/Editora/editora.service.ts
@@ -66,4 +66,35 @@ export class EditoraService {
       }
     }
   }
+
+  // lista editoras por nome
+  public async listByName(
+    edi_Nome: string,
+    page: number,
+    limit: number
+  ): Promise<IPaginatedResponse<EditoraDTO> | ErrorDTO> {
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(`${this.globalService.baseUrl}editora/search`, {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+            // para pular a mensagem do navegador
+            'ngrok-skip-browser-warning': 'true',
+          },
+          params: {
+            edi_Nome: edi_Nome,
+            page: page,
+            limit: limit,
+          },
+        })
+      );
+      return response;
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return new ErrorDTO(error.error.message, error.status);
+      } else {
+        return new ErrorDTO('Erro ao chamar listEditoraByName', 500);
+      }
+    }
+  }
 }
