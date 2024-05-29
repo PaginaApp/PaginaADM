c80adf541d3aed94457b1ab7c1c47e42cb56d375
nathan ks <nathanks0210@gmail.com>
2024-05-29 17:40:48 -0300

    refactor: Update autenticacao.guard.ts for token verification and redirection

--- src/app/_services/guards/autenticacao.guard.ts
+++ src/app/_services/guards/autenticacao.guard.ts
@@ -1,11 +1,36 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { LoginService } from '../loguin/login.service';
 
-export const autenticacaoGuard: CanActivateFn = (route, state) => {
+export const autenticacaoGuard: CanActivateFn = async (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const loginService = inject(LoginService);
+  const router = inject(Router);
+
   if (sessionStorage.getItem('accessToken')) {
-    return true;
+    const isLoged = await loginService.verifyToken(
+      sessionStorage.getItem('accessToken')!,
+      JSON.parse(sessionStorage.getItem('user')!).usu_Id
+    );
+
+    if (isLoged === true) {
+      return true;
+    } else {
+      // Se o token não for válido, redirecione para a página de login
+      console.log('Token inválido');
+      await router.navigate(['/login']);
+      return false;
+    }
   } else {
-    const router = new Router();
-    router.navigate(['/login']);
+    console.log('Token não encontrado');
+    await router.navigate(['/login']);
     return false;
   }
 };
