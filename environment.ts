ea7a794d7ed4ac2de82cfe4adf6c615ea6a6d185
nathan ks <nathanks0210@gmail.com>
2024-06-06 15:54:51 -0300

    refactor: Update environment.ts with BASE_URL for test environment

--- environment.ts
+++ environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
-  BASE_URL: 'https://rattler-pleasing-jay.ngrok-free.app/',
+  //prod
+  //BASE_URL: 'https://rattler-pleasing-jay.ngrok-free.app/',
+  //local
   // BASE_URL: 'http://localhost:3333/',
+  //test
+  BASE_URL: 'https://pup-firm-blowfish.ngrok-free.app/',
 };
