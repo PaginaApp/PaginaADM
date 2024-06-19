0e1b1bbf1051147f760078c3d8d8017da8af4d47
nathan ks <nathanks0210@gmail.com>
2024-06-19 18:42:16 -0300

    feat: partial termo component

---
+++ src/app/components/termo/termo/termo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TermoService } from '../../../_services/Termo/termo.service';
+import { ErrorDTO } from '../../../dto/ErrorDTO';
+import { HeaderComponent } from '../../header/header.component';
+
+@Component({
+  selector: 'app-termo',
+  standalone: true,
+  imports: [HeaderComponent, FormsModule],
+  templateUrl: './termo.component.html',
+  styleUrl: './termo.component.css',
+})
+export class TermoComponent implements OnInit {
+  constructor(public termoService: TermoService) {}
+
+  public termo: string = '';
+
+  async ngOnInit(): Promise<void> {
+    const fulltermo = await this.termoService.getActualTermo();
+
+    if (fulltermo instanceof ErrorDTO) {
+      this.termo = fulltermo.message;
+    } else {
+      this.termo = fulltermo.tpr_Texto;
+    }
+  }
+}
