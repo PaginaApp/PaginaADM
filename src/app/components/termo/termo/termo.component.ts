bdef8edf1097dfec5b7bdb7eeaef90ca34aa2038
nathan ks <nathanks0210@gmail.com>
2024-06-19 19:10:29 -0300

    feat: Add modal functionality to termo component

--- src/app/components/termo/termo/termo.component.ts
+++ src/app/components/termo/termo/termo.component.ts
@@ -1,13 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TermoService } from '../../../_services/Termo/termo.service';
 import { ErrorDTO } from '../../../dto/ErrorDTO';
 import { HeaderComponent } from '../../header/header.component';
+import { NewTermoComponent } from '../new-termo/new-termo.component';
 
 @Component({
   selector: 'app-termo',
   standalone: true,
-  imports: [HeaderComponent, FormsModule],
+  imports: [HeaderComponent, FormsModule, NgIf, NewTermoComponent],
   templateUrl: './termo.component.html',
   styleUrl: './termo.component.css',
 })
@@ -24,5 +26,15 @@ export class TermoComponent implements OnInit {
     } else {
       this.termo = fulltermo.tpr_Texto;
     }
+  }
+
+  public modal: boolean = false;
+
+  public openModal(): void {
+    this.modal = true;
+  }
+
+  public closeModal(): void {
+    this.modal = false;
   }
 }
