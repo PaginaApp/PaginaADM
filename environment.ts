2d45411c28b7d36fde086f8a30a5728d005d6766
nathan ks <nathanks0210@gmail.com>
2024-06-20 21:21:29 -0300

    feat: Update environment.ts with correct BASE_URL for production

--- environment.ts
+++ environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   //prod
-  //BASE_URL: 'https://rattler-pleasing-jay.ngrok-free.app/',
+  BASE_URL: 'https://rattler-pleasing-jay.ngrok-free.app/',
   //local
   // BASE_URL: 'http://localhost:3333/',
   //test
-  BASE_URL: 'https://pup-firm-blowfish.ngrok-free.app/',
+  // BASE_URL: 'https://pup-firm-blowfish.ngrok-free.app/',
 };
